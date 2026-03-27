533415626f71cf019da0b8fa4b9966e291253235
Adam <basharelbishry@hotmail.com>
2026-03-27 20:52:29 +0100

    feat:proof pf fidelity

--- src/content/story.ts
+++ src/content/story.ts
@@ -1,7 +1,7 @@
 import type { StoryConfig } from '../types/story'
 
 export const storyConfig: StoryConfig = {
-  title: '',
+  title: 'bro is not sending this to other girls',
   credits: {
     audioSrc: 'scenes/believe.mp3',
     scrollDurationMs: 220000,
